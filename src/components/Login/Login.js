3e4e90d6be7e91f1a60d8223bd44997e37d99b8b
ashiikkhan <ashiksust06@gmail.com>
2022-10-21 01:47:44 +0600

    login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,8 +3,30 @@ import './Login.css';
 
 const Login = () => {
   return (
-    <div>
-      <h2>hello Login </h2>
+    <div className='form-container'>
+      <h2 className='form-title'>Log In Now !</h2>
+      <form className=''>
+        <div className='form-control'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='email'
+            required
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            placeholder='password'
+            required
+          />
+        </div>
+      </form>
     </div>
   );
 };
